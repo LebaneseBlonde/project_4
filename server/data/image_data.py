dca2dd0cef9ed62151fb07c6a2ab99426e3c1dd7
Ollie Anderson <ollieaa17@gmail.com>
2021-03-11 10:19:01 +0000

    seed data

--- server/data/image_data.py
+++ server/data/image_data.py
@@ -1,7 +1,35 @@
 from models.image_model import Image
 
 image_list = [
+    # fox & firkin
     Image(
-
+        image = 'https://b.zmtcdn.com/data/pictures/8/6126238/3dd16e372e8001fb443f3b3e3b150f0e.jpg'
+    ),
+    Image(
+        image = 'https://payitforward.london.gov.uk/uploads/project_images/ee/ff/1030786/1612951473_img_7460.jpg'
+    ),
+    Image(
+        image = 'https://payitforward.london.gov.uk/uploads/project_images/ee/ff/1030786/1611242371_26678019_1964281923826920_1593465260182464027_o.jpg'
+    ),
+    # Jazz cafe
+    Image(
+        image = 'https://www.crowdfunder.co.uk/uploads/project_images/66/2f/895408/1604338171_201017_jazz_cafe_etta_bond_rob_jones_76.jpg'
+    ),
+    Image(
+        image = 'https://www.crowdfunder.co.uk/uploads/project_images/66/2f/895408/1604338324_the-jazz-cafe-justindesouza-47.jpg'
+    ),
+    # Yuu kitchen
+    Image(
+        image = 'https://payitforward.london.gov.uk/uploads/project_images/0d/6a/951014/1600967903_yuu_mayfair_eggplant_01-hero_with_ta_logo.jpg'
+    ),
+    Image(
+        image = 'https://payitforward.london.gov.uk/uploads/project_images/0d/6a/951014/1600968004_yuu_mayfair_tuna_tostada_with_ta_logo.jpg'
+    ),
+    # skandihus
+    Image(
+        image = 'https://payitforward.london.gov.uk/uploads/project_images/83/1b/1049509/1612636472_whatsapp_image_2021-02-06_at_18.26.56.jpeg'
+    ),
+    Image(
+        image = 'https://payitforward.london.gov.uk/uploads/project_images/83/1b/1049509/1612635015_img_1325.jpg'
     )
 ]
---
+++ server/data/pledge_data.py
@@ -0,0 +1,32 @@
+from models.pledge_model import Pledge
+
+pledge_list = [
+    
+    Pledge(
+        amount = 5
+    ),
+    Pledge(
+        amount = 25
+    ),
+    Pledge(
+        amount = 50
+    ),
+    Pledge(
+        amount = 10
+    ),
+    Pledge(
+        amount = 5
+    ),
+    Pledge(
+        amount = 25
+    ),
+    Pledge(
+        amount = 50
+    ),
+    Pledge(
+        amount = 25
+    ),
+    Pledge(
+        amount = 50
+    ),
+]
