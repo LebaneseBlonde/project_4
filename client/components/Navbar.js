bd56579ac29216badfffc4ae441b6e5e94a561ea
Cal Richards <calrichards@me.com>
2021-03-16 15:55:49 +0000

    navbar tweak

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
     <nav className="navbar is-light is-transparent" role="navigation" aria-label="main navigation">
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
-          <Link className="navbar-item" to='/'><a> Home </a></Link>
+          <Link className="navbar-item" to='/'><div> Home </div></Link>
           <div className="navbar-item has-dropdown is-hoverable">
             <a className="navbar-link"> Your account </a>
             <div className="navbar-dropdown">
