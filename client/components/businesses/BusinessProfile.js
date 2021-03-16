5712ad281d3ca5d05bd70485f2a8b18d593ac122
Cal Richards <calrichards@me.com>
2021-03-16 22:07:33 +0000

    renamed componenets businesss profile

--- client/components/businesses/BusinessProfile.js
+++ client/components/businesses/BusinessProfile.js
@@ -4,14 +4,16 @@ import axios from 'axios'
 
 import BioSection from './BioSection'
 import EndorsementsSection from './EndorsementsSection'
-import FundSection from './FundSection'
+import HeroSection from './HeroSection'
 import UpdatesSection from './UpdatesSection'
+import TiersSection from './TiersSection'
 
 export default function BusinessProfile() {
 
   return <div>
+    <HeroSection />
+    <TiersSection />
     <BioSection />
-    <FundSection />
     <UpdatesSection />
     <EndorsementsSection />
   </div>
