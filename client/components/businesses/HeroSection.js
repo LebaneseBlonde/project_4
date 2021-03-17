ed0f64eef5fc95d6aee90648cd75d86513897600
Cal Richards <calrichards@me.com>
2021-03-17 11:25:06 +0000

    import propps on hero section bp

--- client/components/businesses/HeroSection.js
+++ client/components/businesses/HeroSection.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-export default function HeroSection() {
+export default function HeroSection({ business, loading }) {
 
   
 
