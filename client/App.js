b2986078c97482dbee9b401698249c3332ac9d51
Ollie Anderson <ollieaa17@gmail.com>
2021-03-17 17:37:59 +0000

    updates section

--- client/App.js
+++ client/App.js
@@ -5,8 +5,8 @@ import './styles/style.scss'
 import './styles/homePageStyle.scss'
 import './styles/searchStyle.scss'
 import './styles/bioStyle.scss'
-
-
+import './styles/businessProfileStyle.scss'
+import './styles/updateStyle.scss'
 
 
 
