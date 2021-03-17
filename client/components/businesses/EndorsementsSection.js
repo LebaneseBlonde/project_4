ea64eec22b28de8885b5fb39afac3167bdfe6332
Cal Richards <calrichards@me.com>
2021-03-17 23:43:51 +0000

    endorsements fully functional

--- client/components/businesses/EndorsementsSection.js
+++ client/components/businesses/EndorsementsSection.js
@@ -2,13 +2,59 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-export default function EndorsementsSection({ business, loading, isBusiness}) {
+export default function EndorsementsSection({ business, loading, isBusiness, updateBusiness}) {
   if (loading) return <div><p>loading</p></div>
+  
+  const [text, setText] = useState({content: ''})
+
+  function updateText(event) {
+    setText({...text, [event.target.name]: event.target.value})
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const token = localStorage.getItem('token')
+    try {
+      const {data} = await axios.post(`/api/business/${business.id}/funds/${business.fund[0].id}/endorsements`, text, {
+        headers: { Authorization: `Bearer ${token}`}
+      })
+      updateBusiness(data)
+      setText({content: ''})
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   return <div>
     {business.fund[0].endorsements.map(endorsement => {
       return <div key={endorsement.id}>
         <div>{endorsement.content}</div>
       </div>
     })}
+    <article className="media">
+      <div className="media-content">
+        <div className="field">
+          <p className="control">
+            <textarea
+              className="textarea"
+              placeholder="Make a comment.."
+              onChange={(event) => updateText(event)}
+              value={text.content}
+              name='content'
+            >
+              {text}
+            </textarea>
+          </p>
+        </div>
+        <div className="field">
+          <div className="control">
+            <button
+              onClick={(event) => handleSubmit(event)}
+              className="button is-warning"
+            > Send </button>
+          </div>
+        </div>
+      </div>
+    </article>
   </div>
 }
