5c5509f4b45d25cf07a5301d19feb2078ece27ef
Ollie Anderson <ollieaa17@gmail.com>
2021-03-17 10:57:48 +0000

    singlebusiness API fetch

--- client/components/businesses/BusinessProfile.js
+++ client/components/businesses/BusinessProfile.js
@@ -8,13 +8,28 @@ import HeroSection from './HeroSection'
 import UpdatesSection from './UpdatesSection'
 import TiersSection from './TiersSection'
 
-export default function BusinessProfile() {
+export default function BusinessProfile({match}) {
+
+  const businessId = match.params.businessId
+  const [business, updateBusiness] = useState([])
+  const [loading, updateLoading] = useState(true)
+
+  useEffect(() => {
+    async function getBusiness() {
+
+      const {data} = await axios.get(`/api/businesses/${businessId}`)
+      updateBusiness(data)
+      updateLoading(false)
+    }
+    getBusiness()
+  }, [])
+    
 
   return <div>
-    <HeroSection />
-    <TiersSection />
+    <HeroSection business={business} loading={loading}/>
+    <TiersSection business={business} loading={loading}/>
     <BioSection business={business} loading={loading}/>
-    <UpdatesSection />
-    <EndorsementsSection />
+    <UpdatesSection business={business} loading={loading}/>
+    <EndorsementsSection business={business} loading={loading}/>
   </div>
 }
