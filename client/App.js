a79d1705bd791acc21d615d8c4b39fddf641bb76
Ollie Anderson <ollieaa17@gmail.com>
2021-03-18 02:40:02 +0000

    merge

--- client/App.js
+++ client/App.js
@@ -24,6 +24,7 @@ import BusinessLogin from './components/businesses/BusinessLogin'
 import BusinessProfile from './components/businesses/BusinessProfile'
 import PaymentPage from './components/PaymentPage'
 import PaymentPage from './components/PaymentPage'
+
 const App = () => {
 
 
